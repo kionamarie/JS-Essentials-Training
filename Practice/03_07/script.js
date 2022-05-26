6ac57bb86cba1f5e323d2c04c6277a11bb8d2360
Kiona Coats <kionacoats@gmail.com>
2022-05-25 21:56:26 -0500

    Learning Objects. Completed practice exercise 03_07.

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,25 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bonsai = {
+    name: "Napalm",
+    height: 1.5,
+    vaseType: "ceramic",
+    health: {
+        hydration: true,
+        deadLeaves: false,
+    },
+    watered: false,
+    toggleWatered: function (wateredStatus){
+        this.watered = wateredStatus;
+    }
+}
+
+console.log("My bonsai's name is", bonsai.name);
+console.log("His height is", bonsai.height);
+console.log("His vase is grey, and it is", bonsai.vaseType);
+console.log("If you were to ask me if he was hydrated, I would say", bonsai.health.hydration);
+console.log("Does", bonsai.name, "have dead leaves?");
+console.log(bonsai.health.deadLeaves);
+ 
